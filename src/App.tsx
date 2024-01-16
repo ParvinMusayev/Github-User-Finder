f4f95c16bcd71c0391ba910d78270feb353b7744
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-16 17:32:28 +0400

    feat: Add GitHub user interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react'
+import React from "react";
+import { defaultUser } from "./types";
+import { IGithubUser } from "./types/user";
 
+const App: React.FC = () => {
+  const [user, setUser] = React.useState<IGithubUser | null>(defaultUser);
+  return <div>hello</div>;
+};
 
-function App() {
- 
-
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
-
-export default App
+export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+import { IGithubUser } from "./user";
+
+export const defaultUser: IGithubUser = {
+  login: "ParvinMusayev",
+  id: 115371722,
+  avatar_url: "https://avatars.githubusercontent.com/u/115371722?v=4",
+  name: "Parvin Musayev",
+  company: null,
+  blog: "devsparvin@gmail.com",
+  location: "Baku Azerbaijan",
+  bio: "Frontend developer",
+  twitter_username: null,
+  public_repos: 12,
+  followers: 4,
+  following: 6,
+  created_at: "2022-10-08T19:02:13Z",
+};
