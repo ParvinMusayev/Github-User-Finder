151664639b67bdc3739c155ff5d7b9f816f2052b
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-22 00:52:18 +0400

    feat: Add InfoItem Component

--- src/components/UserCard/components/InfoItem/index.tsx
+++ src/components/UserCard/components/InfoItem/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const InfoItem:React.FC = () => {
-  return (
-    <div>InfoItem</div>
-  )
+//style
+import css from "./InfoItem.module.scss";
+
+interface IInfoItemProps {
+  icon: React.ReactNode;
+  text?: string | null;
+  isLink?: boolean;
 }
 
-export default InfoItem
+const InfoItem: React.FC<IInfoItemProps> = ({ icon, isLink, text }) => {
+  
+  const currentText = text || "Not Avaliable";
+  let currentHref = "";
+
+  if (isLink) {
+    currentHref = text && text.startsWith("http") ? text : `https://${text}`;
+  }
+
+  return (
+    <div className={css.infoItem}>
+      {icon}
+      <div>
+        {isLink && text ? (
+          <a href={currentHref} target="_blank">
+            {currentText}
+          </a>
+        ) : (
+          currentText
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InfoItem;
